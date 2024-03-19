d16191048e463c837b1eac0c64f2f9ad14adf300
shadab <shadabreza.reza@gmail.com>
2024-03-19 15:12:40 +0530

    git init

---
+++ src/main/java/com/shadab/microservicekafkademowithci/MicroservicekafkademowithciApplication.java
@@ -0,0 +1,13 @@
+package com.shadab.microservicekafkademowithci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicekafkademowithciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicekafkademowithciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadab/microservicekafkademowithci/MicroservicekafkademowithciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadab.microservicekafkademowithci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicekafkademowithciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
